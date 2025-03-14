94835b9a1d3ab32fdc49245935d7236c524716d6
Jayden Lombardi <jayden.lombardi@mymail.champlain.edu>
2025-03-14 18:54:39 -0400

    succesfully generating splines through code, as well as meshes. Need a bit of work on fixing the mesh.normals and mesh.vertices but almost there

--- Assets/Scripts/TrackGeneration/Ideas/Cell.cs
+++ Assets/Scripts/TrackGeneration/Ideas/Cell.cs
@@ -4,55 +4,239 @@ using UnityEngine;
 
 public class Cell : MonoBehaviour
 {
-    [Header("Track Information")]
-    public Vector3 trackStartPos;
-    public Vector3 trackEndPos;
-    public Quaternion trackEndRotation;
-
     [Header("Cell Information")]
-    public Bounds cellBounds;
-    public Vector3 cellPosition;
-    public Terrain cellTerrain;
-
-    public void init()
+    //Values that work, but create a square with missing reflections and shadings
+    public Vector3[] vertices = new Vector3[]
     {
-        GameObject cube = new GameObject("Procedural Cube");
+            // Bottom face
+            new Vector3(1f, -1f, 1f), new Vector3(-1f, -1f, 1f),
+            new Vector3(1f, -1f, -1f), new Vector3(-1f, -1f, -1f),
+            // Top face
+            new Vector3(1f, 1f, 1f), new Vector3(-1f, 1f, 1f),
+            new Vector3(1f, 1f, -1f), new Vector3(-1f, 1f, -1f),
+    };
+    public Vector3[] normals = new Vector3[]
+    {
+            // Bottom Face
+            new Vector3(0f, -1f, 0f), new Vector3(0f, -1f, 0f),
+            new Vector3(0f, -1f, 0f), new Vector3(0f, -1f, 0f),
+            // Top Face
+            new Vector3(0f, 1f, 0f), new Vector3(0f, 1f, 0f),
+            new Vector3(0f, 1f, 0f), new Vector3(0f, 1f, 0f),
+            // Front Face
+            new Vector3(0f, 0f, 1f), new Vector3(0f, 0f, 1f),
+            new Vector3(0f, 0f, 1f), new Vector3(0f, 0f, 1f),
+            // Back Face
+            new Vector3(0f, 0f, -1f), new Vector3(0f, 0f, -1f),
+            new Vector3(0f, 0f, -1f), new Vector3(0f, 0f, -1f),
+            // Left Face
+            new Vector3(-1f, 0f, 0f), new Vector3(-1f, 0f, 0f),
+            new Vector3(-1f, 0f, 0f), new Vector3(-1f, 0f, 0f),
+            //Right Face
+            new Vector3(1f, 0f, 0f), new Vector3(1f, 0f, 0f),
+            new Vector3(1f, 0f, 0f), new Vector3(1f, 0f, 0f),
+    };
+    public Vector2[] uvs = new Vector2[]
+    {
+            // Bottom face
+            new Vector2(0, 1), new Vector2(0, 0),
+            new Vector2(1, 1), new Vector2(1, 0),
+            // Top face
+            new Vector2(0, 1), new Vector2(0, 0),
+            new Vector2(1, 1), new Vector2(1, 0),
+    };
+
+    //values that make the created square hold the same values/memory space as one generated through regular means
+    /*    public Vector3[] vertices = new Vector3[]
+        {
+            // Bottom face 
+            new Vector3(1f, -1f, 1f), new Vector3(-1f, -1f, 1f), 
+            new Vector3(1f, -1f, -1f), new Vector3(-1f, -1f, -1f),
+            // Top face 
+            new Vector3(1f, 1f, 1f), new Vector3(-1f, 1f, 1f), 
+            new Vector3(1f, 1f, -1f), new Vector3(-1f, 1f, -1f), 
+            // Front face 
+            new Vector3(1f, -1f, 1f), new Vector3(-1f, -1f, 1f), 
+            new Vector3(1f, 1f, 1f), new Vector3(-1f, 1f, 1f),  
+            // Back face 
+            new Vector3(1f, -1f, -1f), new Vector3(-1f, -1f, -1f),
+            new Vector3(1f, 1f, -1f), new Vector3(-1f, 1f, -1f), 
+            // Left face
+            new Vector3(-1f, -1f, 1f), new Vector3(-1f, -1f, -1f),
+            new Vector3(-1f, 1f, 1f), new Vector3(-1f, 1f, -1f), 
+            // Right face 
+            new Vector3(1f, -1f, 1f), new Vector3(1f, -1f, -1f), 
+            new Vector3(1f, 1f, 1f), new Vector3(1f, 1f, -1f), 
+        };
+        public Vector3[] normals = new Vector3[]
+        {
+            // Bottom face 
+            new Vector3(0f, -1f, 0f), new Vector3(0f, -1f, 0f),
+            new Vector3(0f, -1f, 0f), new Vector3(0f, -1f, 0f),
+            // Top face 
+            new Vector3(0f, 1f, 0f), new Vector3(0f, 1f, 0f),
+            new Vector3(0f, 1f, 0f), new Vector3(0f, 1f, 0f),
+            // Front face 
+            new Vector3(0f, 0f, 1f), new Vector3(0f, 0f, 1f),
+            new Vector3(0f, 0f, 1f), new Vector3(0f, 0f, 1f),
+            // Back face 
+            new Vector3(0f, 0f, -1f), new Vector3(0f, 0f, -1f),
+            new Vector3(0f, 0f, -1f), new Vector3(0f, 0f, -1f),
+            // Left face
+            new Vector3(-1f, 0f, 0f), new Vector3(-1f, 0f, 0f),
+            new Vector3(-1f, 0f, 0f), new Vector3(-1f, 0f, 0f),
+            // Right face
+            new Vector3(1f, 0f, 0f), new Vector3(1f, 0f, 0f),
+            new Vector3(1f, 0f, 0f), new Vector3(1f, 0f, 0f),
+        };
+        public Vector4[] tangents = new Vector4[]
+        {
+            // Bottom face 
+            new Vector4(1f, 0f, 0f, 1f), new Vector4(1f, 0f, 0f, 1f),
+            new Vector4(1f, 0f, 0f, 1f), new Vector4(1f, 0f, 0f, 1f),
+            // Top face
+            new Vector4(1f, 0f, 0f, 1f), new Vector4(1f, 0f, 0f, 1f),
+            new Vector4(1f, 0f, 0f, 1f), new Vector4(1f, 0f, 0f, 1f),
+            // Front face
+            new Vector4(1f, 0f, 0f, 1f), new Vector4(1f, 0f, 0f, 1f),
+            new Vector4(1f, 0f, 0f, 1f), new Vector4(1f, 0f, 0f, 1f),
+            // Back face 
+            new Vector4(1f, 0f, 0f, 1f), new Vector4(1f, 0f, 0f, 1f),
+            new Vector4(1f, 0f, 0f, 1f), new Vector4(1f, 0f, 0f, 1f),
+            // Left face 
+            new Vector4(1f, 0f, 0f, 1f), new Vector4(1f, 0f, 0f, 1f),
+            new Vector4(1f, 0f, 0f, 1f), new Vector4(1f, 0f, 0f, 1f),
+            // Right face 
+            new Vector4(1f, 0f, 0f, 1f), new Vector4(1f, 0f, 0f, 1f),
+            new Vector4(1f, 0f, 0f, 1f), new Vector4(1f, 0f, 0f, 1f),
+        };
+        public Vector2[] uvs0 = new Vector2[]
+        {
+            // Bottom face UV0 
+            new Vector2(0f, 0f), new Vector2(1f, 0f),
+            new Vector2(0f, 1f), new Vector2(1f, 1f),
+            // Top face UV0 
+            new Vector2(0f, 0f), new Vector2(1f, 0f),
+            new Vector2(0f, 1f), new Vector2(1f, 1f),
+            // Front face UV0
+            new Vector2(0f, 0f), new Vector2(1f, 0f),
+            new Vector2(0f, 1f), new Vector2(1f, 1f),
+            // Back face UV0
+            new Vector2(0f, 0f), new Vector2(1f, 0f),
+            new Vector2(0f, 1f), new Vector2(1f, 1f),
+            // Left face UV0
+            new Vector2(0f, 0f), new Vector2(1f, 0f),
+            new Vector2(0f, 1f), new Vector2(1f, 1f),
+            // Right face UV0
+            new Vector2(0f, 0f), new Vector2(1f, 0f),
+            new Vector2(0f, 1f), new Vector2(1f, 1f),
+        };
+        public Vector2[] uvs1 = new Vector2[]
+        {
+            // Bottom face UV1
+            new Vector2(0f, 0f), new Vector2(1f, 0f),
+            new Vector2(0f, 1f), new Vector2(1f, 1f),
+            // Top face UV1
+            new Vector2(0f, 0f), new Vector2(1f, 0f),
+            new Vector2(0f, 1f), new Vector2(1f, 1f),
+            // Front face UV1
+            new Vector2(0f, 0f), new Vector2(1f, 0f),
+            new Vector2(0f, 1f), new Vector2(1f, 1f),
+            // Back face UV1 
+            new Vector2(0f, 0f), new Vector2(1f, 0f),
+            new Vector2(0f, 1f), new Vector2(1f, 1f),
+            // Left face UV1
+            new Vector2(0f, 0f), new Vector2(1f, 0f),
+            new Vector2(0f, 1f), new Vector2(1f, 1f),
+            // Right face UV1 
+            new Vector2(0f, 0f), new Vector2(1f, 0f),
+            new Vector2(0f, 1f), new Vector2(1f, 1f),
+        };*/
+
+    public int[] triangles = new int[]
+    {
+        // Bottom face
+        0, 2, 3,
+        3, 1, 0,
+        // Top face
+        4, 5, 7,
+        7, 6, 4,
+        // Front face
+        0, 1, 5,
+        5, 4, 0,
+        // Back face
+        2, 6, 7,
+        7, 3, 2,
+        // Left face
+        1, 3, 7,
+        7, 5, 1,
+        // Right face
+        0, 4, 6,
+        6, 2, 0,
+    };
+
+    private GameObject cube;
+    private Mesh mesh;
+
+    void Start()
+    {
+        cube = new GameObject("Procedural Cube");
         cube.AddComponent<MeshFilter>();
         cube.AddComponent<MeshRenderer>();
-
-        Mesh mesh = new Mesh();
+        mesh = new Mesh();
         cube.GetComponent<MeshFilter>().mesh = mesh;
+        cube.GetComponent<MeshRenderer>().material = new Material(Shader.Find("Standard"));
 
-        // vertices
-        Vector3[] vertices = 
-            {
-            new Vector3(-25, -25, -10), new Vector3(25, -25, -10), new Vector3(25, 25, -10), new Vector3(-25, 25, -10), 
-            new Vector3(-25, -25, 10), new Vector3(25, -25, 10), new Vector3(25, 25, 10), new Vector3(-25, 25, 10)  
+        UpdateCell();
+    }
+    private void OnValidate()
+    {
+        UpdateCell();
+    }
+
+    public void UpdateCell()
+    {
+/*        // vertices
+        vertices = new Vector3[]
+        {
+            new Vector3 (1f, 0f, 1f),
+            new Vector3 (-1f, 0f, 1f),
+            new Vector3 (1f, 0f, -1f),
+            new Vector3 (-1f, 0f, -1f),
         };
 
         // triangles 
-        int[] triangles = 
+        triangles = new int[]
         {
-            0, 2, 1,  0, 3, 2,  // Back
-            4, 5, 6,  4, 6, 7,  // Front
-            0, 1, 5,  0, 5, 4,  // Bottom
-            2, 3, 7,  2, 7, 6,  // Top
-            0, 4, 7,  0, 7, 3,  // Left
-            1, 2, 6,  1, 6, 5   // Right
+            0, 2, 3,    //1st 
+            3, 1, 0     //2nd
+        };
+
+        // uvs
+        uvs = new Vector2[]
+        {
+            new Vector2 (0, 1),
+            new Vector2 (0, 0),
+            new Vector2 (1, 1),
+            new Vector2 (1, 0),
         };
 
         // normals 
-        Vector3[] normals = 
-         {
-            Vector3.back, Vector3.back, Vector3.back, Vector3.back, // Back
-            Vector3.forward, Vector3.forward, Vector3.forward, Vector3.forward // Front
-        };
+        normals = new Vector3[]
+        {
+            new Vector3 (0f, 1f, 0f), 
+            new Vector3 (0f, 1f, 0f),
+            new Vector3 (0f, 1f, 0f),
+            new Vector3 (0f, 1f, 0f),
+        };*/
 
+        mesh.Clear();
         mesh.vertices = vertices;
-        mesh.triangles = triangles;
         mesh.normals = normals;
-
-        // material
-        cube.GetComponent<MeshRenderer>().material = new Material(Shader.Find("Standard"));
+        mesh.uv = uvs;
+        //mesh.tangents = tangents;
+        //mesh.uv = uvs1;
+        //mesh.uv2 = uvs2;
+        mesh.triangles = triangles;
     }
 }
--- Assets/Scripts/TrackGeneration/Ideas/SplineTest.cs
+++ Assets/Scripts/TrackGeneration/Ideas/SplineTest.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEditor;
 using UnityEngine;
@@ -7,46 +8,41 @@ public class SplineTest : MonoBehaviour
 {
     [Header("Spline Info")]
     public SplineContainer splineContainer;
-    public Spline spline;
 
-    public BezierKnot knot1;
-    public BezierKnot knot2;
-    public BezierKnot knot3;
-    public BezierKnot knot4;
+    [Header("Knot Positions")]
+    public float3 p1 = new float3(1.0f, 0.0f, 1.0f);
+    public float3 p2 = new float3(1.0f, 0.0f, -1.0f);
+    public float3 p3 = new float3(-1.0f, 0.0f, -1.0f);
+    public float3 p4 = new float3(-1.0f, 0.0f, 1.0f);
 
-    public BezierCurve curve1;
-    public BezierCurve curve2;
-    public BezierCurve curve3;
-    public BezierCurve curve4;
-
-    private float3 p1 = new float3(5.0f, 1.0f, 5.0f);
-    private float3 p2 = new float3(5.0f, 1.0f, -5.0f);
-    private float3 p3 = new float3(-5.0f, 1.0f, -5.0f);
-    private float3 p4 = new float3(-5.0f, 1.0f, 5.0f);
-
-    private float3 tangentIn    = new float3(-2.5);
-    private float3 tangentOut   = new float3(2.5);
+    [Header("Knot Tangents")]
+    public float3 tangentIn    = new float3(-0.4714046);
+    public float3 tangentOut   = new float3(0.4714046);
 
     void Start()
     {
-        spline = splineContainer.spline;
+        UpdateSpline();
+    }
+    private void OnValidate()
+    {
+        UpdateSpline();
+    }
 
-        //BezierKnot(float3 positon, float3 tangentIn, float3 tangentOut, quaternion rotation)
-        knot1 = new BezierKnot(p1, tangentIn, tangentOut, new Quaternion(9, 135, 2, 1));
-        knot2 = new BezierKnot(p2, tangentIn, tangentOut, new Quaternion(1, 225, 359, 1));
-        knot3 = new BezierKnot(p3, tangentIn, tangentOut, new Quaternion(0, 315, 0, 1));
-        knot4 = new BezierKnot(p4, tangentIn, tangentOut, new Quaternion(0, 45, 1.5f, 1));
+    public void UpdateSpline()
+    {
+        List<BezierKnot> totalKnots = new List<BezierKnot>
+        {
+            new BezierKnot(p1, tangentIn, tangentOut, quaternion.identity),
+            new BezierKnot(p2, tangentIn, tangentOut, quaternion.identity),
+            new BezierKnot(p3, tangentIn, tangentOut, quaternion.identity),
+            new BezierKnot(p4, tangentIn, tangentOut, quaternion.identity),
+        };
 
-        spline.Add(knot1);
-        spline.Add(knot2);
-        spline.Add(knot3);
-        spline.Add(knot4);
-    
-        curve1 = new BezierCurve(p1, p2);
-        curve2 = new BezierCurve(p2, p3);
-        curve3 = new BezierCurve(p3, p4);
-        curve4 = new BezierCurve(p4, p1);
+        splineContainer.Spline = new Spline(totalKnots, closed: true);
 
-        //spline.closed.set = true;
+        for (int i = 0; i < splineContainer.Spline.Count; i++)
+        {
+            splineContainer.Spline.SetTangentMode(i, TangentMode.AutoSmooth);
+        }
     }
 }
