82ea37ddaf4fcdaa86455eea351948d99c96bf40
Drew Herlocher <drew.herlocher@mymail.champlain.edu>
2025-04-18 11:13:29 -0400

    forgot to stage my files again im actually a bozo :(

--- Assets/Scripts/Car/CarState.cs
+++ Assets/Scripts/Car/CarState.cs
@@ -12,6 +12,16 @@ public class CarState : MonoBehaviour
 
     [SerializeField] private bool isPaused;
 
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip collisionSound;
+    [SerializeField] private AudioClip carDeathSound;
+    [SerializeField] private AudioClip screamSound;
+    [SerializeField] private float minVolume = 0.3f;
+    [SerializeField] private float maxVolume = 1.0f;
+    [SerializeField] private float minPitch = 0.8f;
+    [SerializeField] private float maxPitch = 1.2f;
+
+
     public static Action onCarDeath;
     public static Action onPauseGame;
 
@@ -20,6 +30,8 @@ public class CarState : MonoBehaviour
     {
         isPaused = false;
         health = startingHealth;
+        audioSource = GetComponent<AudioSource>();
+
     }
 
     // Update is called once per frame
@@ -34,10 +46,10 @@ public class CarState : MonoBehaviour
 
         carSpeed = carRB.linearVelocity.magnitude;
 
-/*        if (transform.position.y < -12)
-        {
-            onCarDeath.Invoke();
-        }*/
+        /*        if (transform.position.y < -12)
+                {
+                    onCarDeath.Invoke();
+                }*/
     }
 
     void OnCollisionEnter(Collision collision)
@@ -45,11 +57,18 @@ public class CarState : MonoBehaviour
         if (collision.gameObject.tag == "Obstacle")
         {
             Debug.Log("Obstacle hit");
+
             health -= (int)(carSpeed * speedToHealthFactor);
 
             if (health <= 0)
             {
+                PlayCarDeathSound();
+                PlayScream();
                 onCarDeath.Invoke();
+            }
+            else
+            {
+                PlayCollisionSound();
             }
         }
     }
@@ -57,5 +76,30 @@ public class CarState : MonoBehaviour
     public float GetHealth01()
     {
         return (float)(health) / (float)(startingHealth);
+    }
+
+    public void PlayCollisionSound()
+    {
+        float speedRatio = Mathf.Clamp01(carSpeed / 30f);
+
+        audioSource.volume = Mathf.Lerp(minVolume, maxVolume, speedRatio);
+        audioSource.pitch = Mathf.Lerp(minPitch, maxPitch, speedRatio);
+
+        audioSource.PlayOneShot(collisionSound);
+    }
+
+    private void PlayCarDeathSound()
+    {
+        audioSource.volume = 0.8f;
+        audioSource.pitch = 1.0f;
+
+        audioSource.PlayOneShot(carDeathSound);
+    }
+    private void PlayScream()
+    {
+        audioSource.volume = maxVolume;
+        audioSource.pitch = 1.0f;
+
+        audioSource.PlayOneShot(screamSound);
     }
 }
